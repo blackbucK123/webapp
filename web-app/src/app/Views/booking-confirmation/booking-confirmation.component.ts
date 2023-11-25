b4739cb4315287693726064bcfdbd8f959b2c6fb
blackbucK123 <tanujohari.1312@gmail.com>
2023-11-25 17:16:00 +0530

    Updates

--- web-app/src/app/Views/booking-confirmation/booking-confirmation.component.ts
+++ web-app/src/app/Views/booking-confirmation/booking-confirmation.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';  
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-booking-confirmation',
@@ -9,5 +11,50 @@ import { CommonModule } from '@angular/common';
   styleUrl: './booking-confirmation.component.scss'
 })
 export class BookingConfirmationComponent {
+  closeResult = '';  
+  display: any = 3;
+  constructor(private modalService: NgbModal, private router: Router) {
+    
+  }
+ 
 
+  
+  timer() {
+    let seconds: number = 3;
+    const timer = setInterval(() => {
+      seconds--;
+      this.display = `${Math.floor(seconds)}`;
+      if (seconds == 1) {
+        this.modalService.dismissAll()
+      }else if(seconds == 0){
+        clearInterval(timer);
+        this.router.navigate(['/home']); 
+      }
+    }, 1000);
+  }
+
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+    this.timer();
+    
+  }
+  
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+ 
 }
+function start() {
+  throw new Error('Function not implemented.');
+}
+
